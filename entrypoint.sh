6c65eda4a2340a166612ef7232fdd182e352332a
Lucas Romero <lucas.romero@jimdo.com>
2020-11-07 09:55:29 +0100

    Use variable expansion technique proposed by GitHub docs
    in order to be able to use variables inside the action's args and have
    them correctly replaced

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# expands the arguments in order to pass them to plantuml
-ARGS=( "$@" )
-
-plantuml $ARGS
+# `$*` expands the `args` supplied in an `array` individually 
+# or splits `args` in a string separated by whitespace.
+sh -c "plantuml $*"
